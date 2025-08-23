e94a224d95ce321ee5fa682cec05e3d2d83d1eba
Layomi <mac@MACs-MacBook-Pro.local>
2025-08-23 11:23:30 +0100

    layout, navbar and sidebar implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,60 @@
-function App() {
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
+import "./styles/global.scss";
 
+const Layout = () => {
   return (
-    <div>Hello World</div>
-  )
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menu-container">
+          <Sidebar />
+        </div>
+        <div className="content-container">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          Component: Home,
+          // loader: loadRootData,
+        },
+        {
+          path: "users",
+          Component: Users,
+          // loader: loadRootData,
+        },
+        {
+          path: "products",
+          Component: Products,
+          // loader: loadRootData,
+        },
+      ],
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Iamadmin</span>
+      <span>Iamadmin Dev Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
